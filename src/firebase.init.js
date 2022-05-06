cef716fa7256e74ae06dba11e1afdf193c68d9b7
zillur-rgb <zillurdeu@gmail.com>
2022-05-06 08:08:56 +0200

    env.local added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmNngPt3VB6Qmtv2ziBPnErNQqhH-AlWg",
-  authDomain: "wheel-wave.firebaseapp.com",
-  projectId: "wheel-wave",
-  storageBucket: "wheel-wave.appspot.com",
-  messagingSenderId: "481255529388",
-  appId: "1:481255529388:web:adb813f058a5e3d6c0f809",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
